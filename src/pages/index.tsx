96424a282d77528c8780648c1a1d587f0a444fff
Brad Garropy <bradgarropy@gmail.com>
2021-08-19 15:52:02 -0500

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import SEO from "@bradgarropy/next-seo"
+import Layout from "components/Layout"
+import {FC} from "react"
+
+const IndexPage: FC = () => (
+    <Layout>
+        <SEO title="next starter" />
+        <p>home</p>
+    </Layout>
+)
+
+export default IndexPage
